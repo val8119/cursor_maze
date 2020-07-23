d5a0f6e3a43746bd3de21762e427ea5c26414f28
val8119 <mikk.vahaste@gmail.com>
2020-07-24 00:44:09 +0300

    *

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,29 @@
 // selectors
+const wallCont = document.querySelector(".maze-walls");
+
+const playButton = document.querySelector(".play");
+const playCont = document.querySelector(".play-cont");
+
+const retryButton = document.querySelector(".retry");
+const retryCont = document.querySelector(".retry-cont");
 
 // listeners
+window.onmousemove = logMouseMove;
+playButton.addEventListener("click", startMaze);
 
 // functions
+function logMouseMove(e) {
+    e = event || window.event;
+    if (event.target.parentElement == wallCont) {
+        console.log("hit wall");
+        retryCont.style.display = "block";
+    }
+}
+
+function startMaze() {
+    playCont.style.display = "none";
+}
+
+function retryMaze() {
+    retryCont.style.opacity = "contents";
+}
