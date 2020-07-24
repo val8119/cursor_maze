8a2ec3263126d51b247b89e8de8de529879157b6
val8119 <mikk.vahaste@gmail.com>
2020-07-24 21:22:33 +0300

    Changed button style

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,5 @@
 // selectors
+const body = document.querySelector(".body")
 const wallCont = document.querySelector(".maze-walls");
 
 const playButton = document.querySelector(".play");
@@ -7,23 +8,36 @@ const playCont = document.querySelector(".play-cont");
 const retryButton = document.querySelector(".retry");
 const retryCont = document.querySelector(".retry-cont");
 
+const finishButton = document.querySelector(".finish");
+
+// variables
+var isPlaying = false;
+
 // listeners
 window.onmousemove = logMouseMove;
 playButton.addEventListener("click", startMaze);
+retryButton.addEventListener("click", retryMaze);
 
 // functions
 function logMouseMove(e) {
     e = event || window.event;
-    if (event.target.parentElement == wallCont) {
+    console.log(event.target)
+    if (event.target.parentElement == wallCont && isPlaying || event.target == body && isPlaying) {
         console.log("hit wall");
         retryCont.style.display = "block";
+        finishButton.style.display = "none";
+        isPlaying = false;
     }
 }
 
 function startMaze() {
     playCont.style.display = "none";
+    finishButton.style.display = "block";
+    isPlaying = true;
 }
 
 function retryMaze() {
-    retryCont.style.opacity = "contents";
+    retryCont.style.display = "none";
+    finishButton.style.display = "block";
+    isPlaying = true;
 }
